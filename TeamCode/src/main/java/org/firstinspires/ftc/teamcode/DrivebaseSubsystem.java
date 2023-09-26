31e0fc498f789f8cee3683a1ccc4ee89be8b4f5a
Jay <jaykang445@gmail.com>
2023-09-26 11:43:09 -0700

    some setup for odometry + ftcdashboard
    added some stuff for odometry in drivebase constructor and added ftcdashboard dep. for opmode control + telemetry/stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivebaseSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivebaseSubsystem.java
@@ -2,8 +2,15 @@ package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.geometry.Pose2d;
+import com.arcrobotics.ftclib.geometry.Rotation2d;
+import com.arcrobotics.ftclib.geometry.Translation2d;
+import com.arcrobotics.ftclib.hardware.GyroEx;
+import com.arcrobotics.ftclib.hardware.RevIMU;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.MecanumDriveKinematics;
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.MecanumDriveOdometry;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -15,8 +22,16 @@ public class DrivebaseSubsystem extends SubsystemBase {
     private HardwareMap hMap;
     private Telemetry tele;
 
+    private Pose2d robotPose = new Pose2d();
+
     private Motor fL,fR,bL,bR;
+    private GyroEx gyro;
     private MecanumDrive mecanum;
+    private MecanumDriveKinematics kinematics;
+    private MecanumDriveOdometry odometry;
+
+    //FIXME placeholders. put actual positions once get finalized robot
+    private Translation2d[] motorPositions = {new Translation2d(), new Translation2d(), new Translation2d(), new Translation2d()};
 
     public DrivebaseSubsystem(HardwareMap hMap, Telemetry tele) {
         this.hMap = hMap;
@@ -25,9 +40,29 @@ public class DrivebaseSubsystem extends SubsystemBase {
         fL = new MotorEx(hMap, "fL");
         fR = new MotorEx(hMap, "fR");
         bL = new MotorEx(hMap, "bL");
-        bR = new MotorEx(hMap, "bR");
+        bR = new MotorEx (hMap, "bR");
+
+        gyro = new RevIMU(hMap);
+        gyro.init();
 
         mecanum = new MecanumDrive(true, fL, fR, bL, bR);
+
+        kinematics =
+                new MecanumDriveKinematics(
+                        motorPositions[0],
+                        motorPositions[1],
+                        motorPositions[2],
+                        motorPositions[3]
+                );
+
+        odometry =
+                new MecanumDriveOdometry(
+                        kinematics,
+                        Rotation2d.fromDegrees(0),
+                        //FIXME placeholder starting values. have to fix for actual comp
+                        new Pose2d(1, 2, Rotation2d.fromDegrees(0))
+                );
+
     }
 
     public void driveMotors(DoubleSupplier leftY, DoubleSupplier leftX, DoubleSupplier rightX) {
