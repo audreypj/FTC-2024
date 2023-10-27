5f4dfb03e7dd10ff97468d488e99fbf068e67454
Jay <jaykang445@gmail.com>
2023-10-26 23:08:45 -0700

    intake stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSubsystem.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.hardware.motors.CRServo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class IntakeSubsystem extends SubsystemBase {
+
+    public enum Modes {
+        INTAKE, OUTTAKE, OFF
+    }
+
+    private CRServo intakeServo;
+
+    private Modes currentMode;
+
+    public IntakeSubsystem(HardwareMap hMap) {
+
+        intakeServo = new CRServo(hMap, "intakeServo");
+
+        currentMode = Modes.OFF;
+
+    }
+
+    public void setMode(Modes mode) {
+        currentMode = mode;
+    }
+
+    private void applyMode() {
+        //FIXME placeholder values
+        switch(currentMode) {
+            case INTAKE:
+                intakeServo.set(0);
+                break;
+            case OUTTAKE:
+                intakeServo.set(0);
+                break;
+            case OFF:
+            default:
+                intakeServo.set(0);
+        }
+    }
+
+    @Override
+    public void periodic() {
+
+        applyMode();
+
+    }
+}
