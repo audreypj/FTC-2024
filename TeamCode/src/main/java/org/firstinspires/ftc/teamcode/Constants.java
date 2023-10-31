c4b0f6dca523aad24a1381870d8b34a5db1c9307
Jay <jaykang445@gmail.com>
2023-10-30 21:10:21 -0700

    more arm math and stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -7,4 +7,19 @@ public final class Constants {
         public static final boolean SHOW_DEBUG_DATA = false;
     }
 
+    public static final class Arm {
+        //FIXME placeholders
+        public static final double ARM_GEAR_RATIO = 0;
+        public static final double ARM_SMALL_SPROCKET_CIRCUMFERENCE = 0;
+        public static final double ARM_LARGE_SPROCKET_CIRCUMFERENCE = 0;
+
+        public static final class Setpoints {
+            //FIXME very important fix pls
+            public static final double PARALLEL = 0;
+            public static final double BARE_MIN = -0;
+        }
+
+
+
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ArmSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ArmSubsystem.java
@@ -1,24 +1,64 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.controller.PIDController;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.Constants;
+
+import java.util.Optional;
 
 public class ArmSubsystem extends SubsystemBase {
 
     private HardwareMap hMap;
 
-    private Motor armMotor;
+    private MotorEx armMotor, armMotorTwo;
+
+    private PIDController armController;
+
+    public class ArmState {
+        public double angle, extension;
+        public ArmState(Optional<Double> angle, Optional<Double> extension) {
+            this.angle = angle.orElse((double) 0);
+            this.extension = extension.orElse((double) 0); //FIXME should actually like make these valid default points
+        }
+
+
+    }
 
     public ArmSubsystem(HardwareMap hMap) {
 
         this.hMap = hMap;
 
-        armMotor = new MotorEx(hMap, "armMotor");
+        armMotor = new MotorEx(hMap, "armMotor", Motor.GoBILDA.RPM_60);
+        armMotorTwo = new MotorEx(hMap, "armMotorTwo", Motor.GoBILDA.RPM_60);
+
+        armMotor.resetEncoder();
+        armMotorTwo.resetEncoder();
+
+        armMotorTwo.setInverted(true);
+
+        armController = new PIDController(0.003, 0.001, 0.001);
+        armController.setTolerance(3);
 
     }
 
+    private double tickToDegrees(double ticks) {
+        //FIXME make sure math works
+        return ((ticks / 2786) * Constants.Arm.ARM_GEAR_RATIO) * 360;
+        //currentPos/total circumference = degrees/360
+        //currentPos = (tickPos / cpr) * gear ratio * totalCircumferece
+    }
+
+    private double getRawAnglePosition() {
+        return 0;
+    }
+
+    public double getCorrectedAnglePosition() {
+        return 0;
+    }
 
     @Override
     public void periodic() {
