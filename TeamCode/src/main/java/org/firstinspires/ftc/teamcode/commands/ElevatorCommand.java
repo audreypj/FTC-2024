d9cfa8b5e158aa67e8f0c192b99e0765961f0a42
Jay <jaykang445@gmail.com>
2023-11-17 23:55:05 -0800

    clean up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/ElevatorCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/ElevatorCommand.java
@@ -9,39 +9,15 @@ import java.util.Optional;
 
 public class ElevatorCommand extends CommandBase {
 
-    public static class ScoreState {
-        public double extension, angle;
-
-        public ScoreState(Optional<Double> extension, Optional<Double> angle) {
-            this.extension = extension.orElse(Constants.Elevator.Setpoints.MIN_EXTENSION_INCHES);
-            this.angle = angle.orElse(Constants.Wrist.Setpoints.STOWED);
-            if(extension.isPresent()) { this.extension = extension.get(); }
-            if(angle.isPresent()) { this.angle = angle.get(); }
-        }
-
-        public ScoreState(double extension, double angle) {
-            this(Optional.of(extension), Optional.of(angle));
-        }
-
-        public ScoreState(double extension) {
-            this(Optional.of(extension), Optional.empty());
-        }
-    }
-
     private final ElevatorSubsystem elevatorSubsystem;
 
-    private double extension, angle;
+    private double extension;
 
-    public ElevatorCommand(ElevatorSubsystem elevatorSubsystem, double extension, double angle) {
+    public ElevatorCommand(ElevatorSubsystem elevatorSubsystem, double extension) {
         this.elevatorSubsystem = elevatorSubsystem;
         this.extension = extension;
-        this.angle = angle;
 
         addRequirements(elevatorSubsystem);
-    }
-
-    public ElevatorCommand(ElevatorSubsystem elevatorSubsystem, ScoreState scoreState) {
-        this(elevatorSubsystem, scoreState.extension, scoreState.angle);
     }
 
     @Override
@@ -58,5 +34,4 @@ public class ElevatorCommand extends CommandBase {
     public boolean isFinished() {
         return elevatorSubsystem.atTargetElevator();
     }
-
 }
